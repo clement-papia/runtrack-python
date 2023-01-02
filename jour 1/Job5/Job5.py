6c8a983afb87a997a7656ee51b2675b5f6f3f026
clement-papia <clement.papia@laplateforme.io>
2023-01-02 13:45:44 +0100

    Runtrack-python
    Rendu Runtrack-python jour 1

---
+++ jour 1/Job5/Job5.py
@@ -0,0 +1,4 @@
+print("abcdefghijklmnopqrstuvwxyz")
+str="abcdefghijklmnopqrstuvwxyz"
+reversed_string=''.join(reversed(str))
+print("à l'envers", reversed_string)  
---
+++ jour 1/Pour allez plus loin/Plusloin.py
@@ -0,0 +1,13 @@
+car = "e"
+chaine = "Je vais me préparer à manger"
+if car in chaine: 
+    print ("La lettre -e- se trouve dans la chaîne !") 
+else: 
+    print ("La chaîne ne contient pas la lettre -e- !")
+
+car = "e"
+chaine = "non"
+if car in chaine: 
+    print ("La lettre -e- se trouve dans la chaîne !") 
+else: 
+    print ("La chaîne ne contient pas la lettre -e- !")
